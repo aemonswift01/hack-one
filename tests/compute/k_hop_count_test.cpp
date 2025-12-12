1826dd67bc0c5e204ad134bbc7f938dc9cedf9fc
unknown <362255397@qq.com>
2025-12-12 16:39:58 +0800

    add tests file

---
+++ tests/compute/k_hop_count_test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "src/compute/k_hop_count.cpp"
+
+#include "compute/"
+using namespace std;
+
+int main(){
+    k_hop_count kHopCount;
+    kHopCount.kHopCount();
+    cout << "start helloiiiiiiiii" << endl;
+    return 0;
+}
